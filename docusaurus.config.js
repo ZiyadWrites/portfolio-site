6dd492edde4de1bf3d919acbce62c179fe10c095
Ziyad Allie <zundisputedz@gmail.com>
2025-03-02 09:57:52 +0200

    changed title again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are very cool',
+  tagline: 'Dinosaurs are pretty cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
