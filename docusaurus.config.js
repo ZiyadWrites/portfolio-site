1520f2883048c1ed2207ad075a9cf2f7e0862f6c
Ziyad Allie <zundisputedz@gmail.com>
2025-03-02 10:39:48 +0200

    Updated docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are very cool',
+  title: 'Ziyad Allie',
+  tagline: 'Technical writing portfolio',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
